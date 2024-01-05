b1a2c3019525996b7b49ee9992588732db48aea4
Ritam91 <ritamkumarsinha999@gmail.com>
2024-01-05 05:35:03 +0530

    Header section done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 import './Header.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="header section__padding wrapper" id="home">
+
+    <div className="wrapper__info">
+      <SubHeading title="Your Ultimate Dining Destination" />
+      <h1 className="header__h1">
+      Sip, savor, rejoice, repeat
+      </h1>
+
+      <p className="p__opensans" style={ {margin: '2rem 0'} }>
+      Escape to culinary bliss at FlavorFiesta . Unleash your palate with our uniquely crafted dishes, where each bite is an extraordinary experience.
+      </p>
+
+      <button type='button' className='custom__button'>
+        Explore Menu
+      </button>
+    </div>
+
+    <div className="wrapper__img">
+      <img src= {images.welcome} alt="welcome" />
+    </div>
+
   </div>
 );
 
